d5f84be69c0d8529c85aecf50e73ea9cf9eb112b
Yaroslav Patrikeev <yaroslav.patrikeev@gmail.com>
2023-10-14 20:40:24 +0300

    fix apiPath

--- src/constants/paths.constants.js
+++ src/constants/paths.constants.js
@@ -1,2 +1,1 @@
-// export const apiPath = 'https://api.patrikeev.movies.nomoredomainsrocks.ru/';
-export const apiPath = 'http://localhost:3001/';
+export const apiPath = 'https://api.patrikeev.movies.nomoredomainsrocks.ru/';
